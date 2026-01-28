e54591124f571dd7eb0c4c274cd309ab6118892c
sakhamurisai <contact.sakhamuri.sai@gmail.com>
2026-01-28 09:38:36 -0500

    terra_form_update

--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -1,19 +1,19 @@
 output "bucket_id" {
   description = "ID of the S3 bucket"
-  value       = aws_s3_bucket.frontend.id
+  value       = aws_s3_bucket.frontend_gig.id
 }
 
 output "bucket_name" {
   description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.frontend.id
+  value       = aws_s3_bucket.frontend_gig.bucket
 }
 
 output "bucket_domain_name" {
   description = "Domain name of the S3 bucket"
-  value       = aws_s3_bucket.frontend.bucket_domain_name
+  value       = aws_s3_bucket.frontend_gig.bucket_domain_name
 }
 
 output "bucket_website_endpoint" {
   description = "Website endpoint of the S3 bucket"
-  value       = aws_s3_bucket_website_configuration.frontend.website_endpoint
+  value       = aws_s3_bucket_website_configuration.frontend_gig.website_endpoint
 }

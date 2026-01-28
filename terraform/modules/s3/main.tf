839fcca51fc252e8318abf4c916e334e6333fbcf
sakhamurisai <contact.sakhamuri.sai@gmail.com>
2026-01-28 09:07:34 -0500

    terraform_s3_update

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -5,6 +5,7 @@ resource "aws_s3_bucket" "frontend" {
 
 resource "aws_s3_bucket_versioning" "frontend" {
   bucket = aws_s3_bucket.frontend.id
+  force_destroy = true
   versioning_configuration {
     status = "Enabled"
   }

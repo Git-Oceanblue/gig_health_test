f722e23062129c40b45106c1a976124aeeda90e0
Sai <contact.sakhamuri.sai@gmail.com>
2026-02-02 11:08:10 -0500

    updated lambda handler

--- backend/lambda_handler.py
+++ backend/lambda_handler.py
@@ -3,8 +3,9 @@ from mangum import Mangum
 from main import app
 #keep this for local testing
 os.environ.setdefault('AWS_LAMBDA_FUNCTION_NAME', 'resume-builder-api')
+sys.path.append('.')
 
-handler = Mangum(app, lifespan="off")
+handler = Mangum(app, lifespan="on")
 
 def lambda_handler(event, context):
     
